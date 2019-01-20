b64bf154de26ca3f24132387d3d4ccf83daef80f
Jordan Poles <jpoles1@gmail.com>
2019-01-20 13:24:02 -0500

    More cleaning!

--- certbot-add.sh
+++ certbot-add.sh
@@ -1,2 +1,1 @@
 docker exec -it certbot-nginx bash -c 'certbot --authenticator standalone --installer nginx --pre-hook "nginx -s stop"'
-./nginx-restart.sh
--- certbot-renew.sh
+++ certbot-renew.sh
@@ -1,2 +1,1 @@
 docker exec -it certbot-nginx bash -c 'certbot renew --authenticator standalone --installer nginx --pre-hook "nginx -s stop"'
-./nginx-restart.sh
